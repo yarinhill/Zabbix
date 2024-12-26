9fa48f2ff1f0fedd8681d541c21174ba12d1d58d
Yarin <yarin.hill@yahoo.com>
2024-12-26 14:15:50 +0200

    Zabbix

--- agent/agent.sh
+++ agent/agent.sh
@@ -7,10 +7,6 @@ read -p "Enter your OS Version (9,8..): " OS_VERSION
 
 # Install Zabbix repository
 sudo rpm -Uvh "https://repo.zabbix.com/zabbix/${ZABBIX_VERSION}/${OS_DISTRO}/${OS_VERSION}/x86_64/zabbix-release-latest-${ZABBIX_VERSION}.el${OS_VERSION}.noarch.rpm"
-if [ $? -ne 0 ]; then
-    echo "Failed to add Zabbix repository. Please check your inputs and try again."
-    exit 1
-fi
 
 # Clean up metadata and install Zabbix agent
 sudo dnf clean all -y
